51da0fd801beffd1646ca851d004d504eb1ebd58
shwti <81630660+shwti@users.noreply.github.com>
2021-07-25 19:52:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /* Your Bot Default Prefix For Every Server */
-exports.Default_Prefix = "";
+exports.Default_Prefix = "f.";
 /* Your Bot All Embeds Color */
-exports.Color = "";
+exports.Color = "pink";
 /* Your Bot Token (Secret) */
-exports.Token = "";
+exports.Token = "ODY4ODk4MTMyNzkxMDIxNTg5.YP2Wcg.I5bTtIokaIcVAeQJMAisBwmoFVw";
 /* Bot's Owner ID */
-exports.OwnerID = "";
+exports.OwnerID = "792386136756584468";
